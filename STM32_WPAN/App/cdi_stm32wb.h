2c2052d9614dd320afe58c9f225f8d9ad4d7eb27
phreakazone <phreakazone@gmail.com>
2026-09-11 19:03:13 +0700

    Delete STM32_WPAN directory

--- STM32_WPAN/App/cdi_stm32wb.h
+++
@@ -1,6 +0,0 @@
-#ifndef CDI_STM32WB_H
-#define CDI_STM32WB_H
-#include <stdbool.h>
-#include <stdint.h>
-void CDI_STM32WB_SetConnected(uint16_t handle, bool connected);
-#endif
--- STM32_WPAN/App/p2p_stub.c
+++
@@ -1,5 +0,0 @@
-#include <stdint.h>
-#include "p2p_server_app.h"
-void P2PS_APP_Init(void){}
-void P2PS_APP_Notification(P2PS_APP_ConnHandle_Not_evt_t *p){(void)p;}
-void P2PS_APP_SW1_Button_Action(void){}
