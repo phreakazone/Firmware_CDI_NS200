d12431f9cf596b2a71b5af01ab6132ddc56a20df
phreakazone <phreakazone@gmail.com>
2026-09-11 19:03:02 +0700

    Delete Core directory

--- Core/Src/cdi_irq.c
+++
@@ -1,5 +0,0 @@
-#include "cdi_board.h"
-void TIM2_IRQHandler(void){ HAL_TIM_IRQHandler(&htim2); }
-/* DMA ADC berjalan circular; flag dibersihkan HAL melalui handle yang diekspor. */
-void DMA1_Channel1_IRQHandler(void){ HAL_DMA_IRQHandler(hadc1.DMA_Handle); }
-void EXTI3_IRQHandler(void){ HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_3); }
